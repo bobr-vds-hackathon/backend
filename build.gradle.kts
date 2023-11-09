326a73683ea8feeaffa6b773c4561f14e6289984
argraur <me@argraur.dev>
2023-11-10 02:31:12 +0300

    Set main class properly x2
    Signed-off-by: argraur <me@argraur.dev>

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ group = "dev.argraur.bobry"
 version = "0.0.1"
 
 application {
-    mainClass.set("io.ktor.server.application.ApplicationKt")
+    mainClass.set("dev.argraur.bobry.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
